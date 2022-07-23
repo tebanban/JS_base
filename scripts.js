d220aecc5ef22562f7c57eaa4553281f894209eb
tebanban <estebancalf@gmail.com>
2022-07-22 22:46:04 -0600

    Add table script

--- scripts.js
+++ scripts.js
@@ -1,13 +1,14 @@
 var i=0
 var number = prompt("Enter a number:");
 while (i <= 12 ){
-    let result = number * i
+    var result = number * i
     console.log( number + " x " + i + "=" + result)
+    const node = document.createElement ("li")
+    const textnode = document.createTextNode( number + " x " + i + "=" + result)
+    node.appendChild(textnode)
+    document.getElementById("demo").appendChild(node)
     i += 1
 }
-
-document.getElementById("demo").innerHTML= number 
-
 
 
 
