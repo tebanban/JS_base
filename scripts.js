20d8475cab108e3faeee046ac4d3b089e67712e7
tebanban <estebancalf@gmail.com>
2022-07-25 21:34:34 -0600

    Add if else example

--- scripts.js
+++ scripts.js
@@ -1,24 +1,47 @@
 var i=0
-var input = document.getElementById("input")
-var title= document.getElementById("title")
+var input = document.getElementById("inputNumber")
+var title= document.getElementById("titleNumber")
 
 input.addEventListener('change', upDate)
 function upDate(e) {
     title.textContent = e.target.value;
     const number = Number(e.target.value)
-    console.log(Number(e.target.value))
-    
+    console.log(Number(number))
+
     while (i <= 12 ){
         var result = number * i
         const node = document.createElement ("li")
         const textnode = document.createTextNode( number + " x " + i + "=" + result)
         node.appendChild(textnode)
-        document.getElementById("demo").appendChild(node)
+        document.getElementById("demoNumber").appendChild(node)
         i += 1
     }
   }
 
 
+//Use of conditional if else
+const select= document.getElementById('weather')
+const pResponse = document.getElementById('pSelect');
+
+select.addEventListener('change', setWeather);
+
+function setWeather (){
+    const choice = select.value;
+
+    if (choice === 'sunny'){
+        pResponse.textContent= "Use of sunglasses is welcome"
+    }
+   else if( choice === 'rainy'){
+    pResponse.textContent= "Bring your umbrella"
+   }
+   else if( choice === 'overcast'){
+    pResponse.textContent = 'No need for sunblock'
+   }
+   else {
+    pResponse.textContent='';
+   }
+}
+ 
 
 
 
